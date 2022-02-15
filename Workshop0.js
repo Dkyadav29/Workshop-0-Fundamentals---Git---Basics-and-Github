af4044d8f0a8f5786c9063b79cf31242fd525469
Dablu <dkyadav2905@gmail.com>
2022-02-15 17:32:20 +0530

    Add on basic mathmetical operation with javascripte

--- Workshop0.js
+++ Workshop0.js
@@ -4,3 +4,18 @@ let commit =' commitements 3'
 
 console.log(commit);
 
+/* for better understand of github try again its basic operation with 
+min javascripte mathmetical operation
+
+*/
+
+let obj = {};
+
+obj.x = 3;
+console.log(obj.x); // Prints 3.
+console.log(obj); // Prints { x: 3 }.
+
+const key = "y";
+obj[key] = 5;
+console.log(obj[key]); // Prints 5.
+console.log(obj); // Prints { x: 3, y: 5 }.
